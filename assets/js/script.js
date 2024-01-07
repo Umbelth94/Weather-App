e2a9ba64d4ff8bf58bd21dfdbc5335c595b0688e
Travis Umbel <Umbelth94@gmail.com>
2024-01-07 17:55:39 -0600

    Made API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,12 @@ var searchButton = document.querySelector('#search-btn');
 var cityInput = document.querySelector('#city-input')
 var savedCitiesContainer = document.querySelector('#saved-cities')
 
+var latitude;
+var longitude;
+
 var cityData = JSON.parse(localStorage.getItem('cityData')) || [];
 
-
+//When button is pressed, 
 searchButton.addEventListener('click',function(){
     saveCity();
     
@@ -22,6 +25,7 @@ function saveCity(){
     cityData.push(inputData);
     localStorage.setItem('cityData',JSON.stringify(cityData));
     displayCities();
+    getWeather(inputData);
     cityInput.value='';
 }
 
@@ -30,14 +34,66 @@ function displayCities(){
     for (let i = 0; i < cityData.length; i++){
         var cityBtn = document.createElement('button');
         cityBtn.addEventListener('click', function(){
-            //Do the API populate function
+            getWeather(cityData[i]);
+            console.log('This is where the' +cityData[i] + 'function goes');
         });
         cityBtn.textContent=cityData[i];
         cityBtn.setAttribute('class', 'btn btn-secondary mt-2');
-        cityBtn.setAttribute
         savedCitiesContainer.append(cityBtn);
         console.log(cityData[i]);
     }
 }
+
+function getCoords(cityName) {
+    return fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=bf70693fc02342902eb9d0f51befef5a')
+        .then(response => response.json())
+        .then(data => {
+            latitude = Math.round(data[0]?.lat * 100) / 100;
+            longitude = Math.round(data[0]?.lon * 100) / 100;
+            console.log(latitude, longitude);
+            return { latitude, longitude };
+        })
+        .catch(error => {
+            console.error('Error getting coordinates:', error);
+            throw error;
+        });
+}
+
+function getWeather(cityName) {
+    getCoords(cityName)
+        .then(({ latitude, longitude }) => {
+            fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=bf70693fc02342902eb9d0f51befef5a`)
+                .then(response => response.json())
+                .then(data => {
+                    const currentUnixTimestamp = dayjs().unix();
+                    console.log('current unix hour ' + currentUnixTimestamp);
+                    
+                    //Loop through all of the indexes to get the current hour's forecast
+                    let closestTimestamp = Infinity;
+                    let closestIndex = -1;
+                    for (i = 0; i < data.list.length; i++){
+                        const timestampDifference = Math.abs(data.list[i].dt - currentUnixTimestamp)
+                        if (timestampDifference < closestTimestamp) {
+                            closestTimestamp = timestampDifference;
+                            closestIndex = i;
+                        }
+                    }
+                        if (closestIndex !== -1){
+                            console.log('Closest weather data:', data.list[closestIndex]);
+                        } else {
+                            console.log('no matching weather data found.');
+                        }
+                    
+                    console.log(data);
+                })
+                .catch(error => {
+                    console.error('Error getting weather data:', error);
+                });
+        })
+        .catch(error => {
+            console.error('Error getting coordinates:', error);
+        });
+}
+
 
 displayCities();
